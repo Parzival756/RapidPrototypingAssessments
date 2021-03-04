77e66b117ae962aabd197ae38ccc569deba7dc7f
Parzival756 <79180455+Parzival756@users.noreply.github.com>
2021-03-04 13:24:40 +1100

    Update PlayerController.cs
    Completed lesson 4.1 player now moves toward Z axis of the focal point.

--- Assets/Prototype1/Scripts/PlayerController.cs
+++ Assets/Prototype1/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed;
+    private Rigidbody PlayerRB;
+    private GameObject FocalPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRB = GetComponent<Rigidbody>();
+        FocalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float forwardInput = Input.GetAxis("Vertical");
+        PlayerRB.AddForce(FocalPoint.transform.forward * speed * forwardInput);
     }
 }
