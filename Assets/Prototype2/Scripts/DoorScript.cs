46b5a444265e9440ca384b8e7243f6cab62d7d64
Parzival756 <79180455+Parzival756@users.noreply.github.com>
2021-03-15 12:16:00 +1100

    Prototype2 Start

---
+++ Assets/Prototype2/Scripts/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject ButtonBox;
+    public GameObject Player;
+    public GameObject Door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ButtonBox.transform.localScale = new Vector3(0, -5, 0);
+        Door.SetActive(false);
+        Debug.Log("HEYA");
+    }
+}
---
+++ Assets/Prototype2/Scripts/TreeLifeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//The idea of this is that the player will press a button and it will refil their timer/health, in small amounts.
+//the tree will also open the door for a certain time allowing the player to enter the next area. (Remanded to a button object)
+
+public class TreeLifeScript : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
