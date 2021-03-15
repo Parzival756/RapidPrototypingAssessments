0a152a1f3afc746f4844612d5382fd99fbb10b33
Parzival756 <79180455+Parzival756@users.noreply.github.com>
2021-03-15 14:32:11 +1100

    Additions to the Treescripts and the Doors.
    STILL NEED TO FIX!

--- Assets/Prototype2/Scripts/DoorScript.cs
+++ Assets/Prototype2/Scripts/DoorScript.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    public GameObject ButtonBox;
     public GameObject Player;
     public GameObject Door;
+    public GameObject Button;
+    public GameObject Buttonbox;
+    public GameObject ButtonTrigger;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +23,11 @@ public class DoorScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        ButtonBox.transform.localScale = new Vector3(0, -5, 0);
-        Door.SetActive(false);
-        Debug.Log("HEYA");
+        if (Buttonbox.gameObject.tag == "player")
+        {
+            Button.transform.position = new Vector3(0, -5, 0);
+            Door.SetActive(false);
+            Debug.Log("HEYA");
+        }
     }
 }
--- Assets/Prototype2/Scripts/TreeLifeScript.cs
+++ Assets/Prototype2/Scripts/TreeLifeScript.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
 
 //The idea of this is that the player will press a button and it will refil their timer/health, in small amounts.
 //the tree will also open the door for a certain time allowing the player to enter the next area. (Remanded to a button object)
@@ -9,6 +11,11 @@ public class TreeLifeScript : MonoBehaviour
 {
 
     public GameObject player;
+    public GameObject TreeTrigger1; 
+    public GameObject TreeTrigger2; 
+    public GameObject TreeTrigger3; 
+    public GameObject TreeTrigger4;
+    public float timeleft = 60.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +25,36 @@ public class TreeLifeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeleft -= Time.deltaTime;
+        if (timeleft <= 0)
+        {
+            GameOver();
+        }
+        TreeTime();
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void TreeTime()
+    {
+        if (TreeTrigger1.gameObject.tag == "player")
+        {
+            timeleft =+ 60f;
+        }
+        else if (TreeTrigger2.gameObject.tag == "player")
+        {
+            timeleft = +60f;
+        }
+        else if (TreeTrigger3.gameObject.tag == "player")
+        {
+            timeleft = +60f;
+        }
+        else if (TreeTrigger4.gameObject.tag == "player")
+        {
+            timeleft = +60f;
+        }
     }
 }
