a366471c60c67a8360f4ac221c1f1a4799bb64fb
Parzival756 <79180455+Parzival756@users.noreply.github.com>
2021-05-13 13:15:01 +1000

    Finalized prototypes

---
+++ Assets/Prototype3/Scripts/Anchors.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Anchors : MonoBehaviour
+{
+    public Player player;
+    public float timeleft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider Floor)
+    {
+        if (gameObject.tag == "Sub")
+        {
+            player.Sub = true;
+        }
+        else if (gameObject.tag == "Dom")
+        {
+            player.Dom = true;
+        }
+    }
+
+     private void OnTriggerExit(Collider other)
+     {
+        if (gameObject.tag == "Sub")
+        {
+            player.Sub = false;
+        }
+        else if (gameObject.tag == "Dom")
+        {
+            player.Dom = false;
+        }
+    }
+}
---
+++ Assets/Prototype3/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+
+    public GameObject Pivot1;
+    public GameObject Pivot2;
+    private Rigidbody Thebod;
+    private bool touchie;
+    public bool Sub, Dom;
+    void Start()
+    {
+       Thebod = GetComponent<Rigidbody>() ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0 && Sub)
+        {
+            transform.RotateAround(Pivot1.transform.position, Vector3.forward, -speed * Time.deltaTime);
+            Thebod.useGravity = false;
+        }
+        else if (Input.GetAxis("Horizontal") < 0 && Dom)
+        {
+            transform.RotateAround(Pivot2.transform.position, Vector3.back, speed * Time.deltaTime);
+            Thebod.useGravity = false;
+        }
+        else
+        {
+            Thebod.useGravity = true;
+            //touchie = false;
+        }
+    }
+
+   /* private void OnTriggerStay(Collider other)
+    {
+        touchie = true;
+        Debug.Log(touchie);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+             
+        touchie = false;
+        
+        Debug.Log(touchie);
+    }*/
+}
